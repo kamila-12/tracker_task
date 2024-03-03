b021d49d1f7fc2adcf611d242256d5e806c07e4a
Kamila <kamieva_k@iuca.kg>
2024-03-03 21:03:51 +0600

    added task_detail, task_edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView, TaskEditView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
-    
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('task/<int:pk>/edit/', TaskEditView.as_view(), name='task_edit'),  # URL для редактирования задачи
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
 from .models import Task
+from django.urls import reverse_lazy, reverse
+from django.utils import timezone
+from django.views.generic import ListView
+from django.views.generic import UpdateView
+from django.views.generic import DetailView
 
 class TaskListView(ListView):
     model = Task
     template_name = 'blog/task_list.html'
     context_object_name = 'tasks'
+    
+class TaskDetailView(DetailView):
+    model = Task 
+    template_name = 'blog/task_detail.html'
+    context_object_name = 'task'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['edit_url'] = reverse('task_edit', kwargs={'pk': self.object.pk})
+        return context
+    
+           
+class TaskEditView(UpdateView):
+    model = Task
+    fields = ['title', 'priority_choice', 'status_choice']  # Поля, которые можно редактировать
+    template_name = 'blog/task_edit.html'
+    success_url = reverse_lazy('task_list')
