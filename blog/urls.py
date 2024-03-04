73bf3fce7fc64bb76dc4eaf4b71e5856157fb3a2
Kamila <kamieva_k@iuca.kg>
2024-03-04 16:10:32 +0600

    added forms, task_new

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TaskEditView
+from .views import TaskListView, TaskDetailView, TaskEditView, TaskCreateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
     path('task/<int:pk>/edit/', TaskEditView.as_view(), name='task_edit'),  # URL для редактирования задачи
+    path('task/edit/', TaskCreateView.as_view(), name='task_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from .models import Task
 from django.urls import reverse_lazy, reverse
 from django.utils import timezone
+from .forms import TaskForm
 from django.views.generic import ListView
 from django.views.generic import UpdateView
 from django.views.generic import DetailView
+from django.views.generic import CreateView
 
 class TaskListView(ListView):
     model = Task
-    template_name = 'blog/task_list.html'
     context_object_name = 'tasks'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'blog/task_edit.html'
+    success_url = reverse_lazy('task_list')
     
+
 class TaskDetailView(DetailView):
     model = Task 
     template_name = 'blog/task_detail.html'
